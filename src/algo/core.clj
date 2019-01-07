5ad686f3d9048801f8dc00d4735711fc4b3d72a4
Emiliano Pedrozo <emi@whitebeard.emiliano.in>
2019-01-06 21:53:17 -0300

    Added utils functions and cleanup

--- src/algo/core.clj
+++ src/algo/core.clj
@@ -6,6 +6,6 @@
 (defn -main []
   (def l (asort/insert_sort '(2 3 1 55 148 658 77 74)))
   (print l)
-  )
+)
 
 (-main)
---
+++ src/algo/utils.clj
@@ -0,0 +1,16 @@
+(ns algo.utils
+  (:gen-class))
+
+(defmacro time
+  "Evaluates expr and prints the time it took.  Returns the value of
+ expr."
+  {:added "1.0"}
+  [expr]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr]
+     (prn (str "Elapsed time: " (/ (double (- (. System (nanoTime)) start#)) 1000000.0) " msecs"))
+     ret#))
+
+(defn simple-sample[size max-element]
+  (take size (repeatedly #(rand-int max-element)))
+)
