b9d7b20e78fccdd37453592221b2d7564b3327aa
Emiliano Pedrozo <emi@whitebeard.emiliano.in>
2019-01-06 21:24:11 -0300

    Added insert-sort algorithm

--- src/algo/core.clj
+++ src/algo/core.clj
@@ -1,5 +1,11 @@
 (ns algo.core
   (:gen-class))
 
+(require '[algo.sort :as asort])
+
 (defn -main []
-  (println "Hello, World!"))
+  (def l (asort/insert_sort '(2 3 1 55 148 658 77 74)))
+  (print l)
+  )
+
+(-main)
---
+++ src/algo/sort.clj
@@ -0,0 +1,25 @@
+(ns algo.sort
+  (:gen-class))
+
+(defn insert_sort [l]
+  "Simple insert-sort algorithm implementation. We use java list because sequences are immutable in Clojure"
+  (with-local-vars [key nil, i nil]
+    (def jarray (into-array Integer/TYPE l))
+    (doseq [x (range 1 (count jarray))]
+      (var-set key (int (nth jarray x)))
+      (var-set i (dec x ))
+      (while
+        (and
+          (> (int @i) -1)
+          (< (int @key)  (nth jarray (int @i)))
+          )
+        (do
+          (aset jarray (inc @i) (nth jarray @i))
+          (var-set i (dec @i))
+        )
+      )
+      (aset jarray (inc @i) @key)
+    )
+    (into [] jarray)
+  )
+)
